0af2dd6ee0cbd7867beb8aa157e58da1179eadc7
Alejandro Chan Bernal <184391@mail.unacar.mx>
2024-04-29 16:38:09 -0600

    Prueba de  chequeo

---
+++ JS/mezcladora.js
@@ -0,0 +1,42 @@
+// Datos de las canciones
+const NombreCancion = [
+    'Nine Point Eight','Chocological',
+    'Unidentified Flavorful Object', 'Space Colony', 'Sl0t', 'Colorful',
+    'Camelia','Lemonade','Extension of You', 'Fossil',
+    'Though Our Path May Diverge',
+    'Petrolea',
+    'Poems of a Machine',
+    'Flowerworks',
+    'Bento Box Bivouac',
+    'Between Two Worlds', 'Fly, My Wings',
+    'Chocological (Key Ingridient)'
+]
+const Albumnes = [
+    'Mag Mell', 'Mag Mell',
+    'Miracle Milk', 'Miracle Milk', 'Miracle Milk', 'Miracle Milk',
+    'Millenium Mother', 'Millenium Mother', 'Millenium Mother', 'Millenium Mother',
+    'Rightfully (TV Animation Goblin Slayer OP)',
+    'Intrauterine Education',
+    'To Kill a Living Book - for Library of Ruina',
+    'Flowerworks (EP)',
+    'Dandelions & Bento Boxes',
+    'Between Two Worlds (EP)', 'Fly, My Wings (Single)',
+    'Key Ingridient' 
+]
+const Duracion = [
+    '3:10', '4:20',
+    '4:04', '3:13', '4:50', '4:02',
+    '4:39', '3:10', '4:51', '3:34',
+    '3:18',  
+    '4:21',
+    '4:34',
+    '3:45',
+    '4:14',
+    '4:55', '3:14',
+    '4:01',
+]
+const Portadas = [
+   
+
+
+]
